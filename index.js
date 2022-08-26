cd58c2a234093d609e217d791332c09ec38e4a1f
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-08-26 09:22:53 +0530

    task2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const product=(a,b)=>a*b;
+console.log(product(4,8));
+
+const student={
+    name:"star",
+    age:20,
+    greet(){
+        console.log('Hi, I am '+this.name);
+    }
+};
+
+console.log(student);
+student.greet();
