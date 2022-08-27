b93a8f88b7009522e40cddec526a12b319a88152
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-08-27 22:10:13 +0530

    Custom responses

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const http=require('http');
 
 const server=http.createServer((req,res)=>{
     console.log("tarun");
+    let str='Welcome to my Node Js project';
+    switch(req.url){
+        case '/home':
+            str='Welcome home';
+            break;
+        case '/about':
+            str='Welcome to About Us page';
+            break;
+        case '/node':
+            str='Welcome to my Node Js project';
+            break;
+
+    }
+    res.write('<html>');
+    res.write(`<body><h1>${str}</h1></body></html>`);
+    res.end();
 });
 
 server.listen(4000);
+
+console.log("hi");
